e17ec2c524b32b319df36dff2689cb8b6d55fd19
syedaraffiyarashid <syeda.raffiyarashid@gmail.com>
2021-11-18 04:57:40 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import logo from "./logo.svg";
+import "./App.css";
+import AppRouter from "./config/router/router";
+
+function App() {
+  return (
+    <div className="App">
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config/router/index.js
@@ -0,0 +1,9 @@
+import Home from "../../container/home";
+import About from "../../container/about";
+import Service from "../../container/service";
+import Contact from "../../container/contact";
+import Profile from "../../container/profile";
+import ErrorPage from "../../container/errorPage";
+import Dashboard from "../../container/dashboard";
+
+export { Home, About, Service, Contact, Profile, ErrorPage, Dashboard };
